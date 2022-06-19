a60c6ff113809e626f02bf1ad5d57266ad5f50ce
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-19 09:45:17 +0300

    Задача №25 решена

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 25: Напишите цикл, который принимает на вход два натуральных числа (A и B) и возводит число A в степень B.
+Console.Write("Введите число А: ");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень В для числа А: ");
+int degree=Convert.ToInt32(Console.ReadLine());
+int Exponentiation(int a, int degree)
+{
+  int result = 1;
+  for(int i=1; i <= degree; i++)
+  {
+    result = result * a;
+  }
+    return result;
+}
+Console.Write($"{a} ^ {degree} = {Exponentiation(a, degree)}");
