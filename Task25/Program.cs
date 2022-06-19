19c44b28285a0927d6072c463abeaa323bbc2ea2
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-19 09:46:28 +0300

    Оформление кода в стиле С#

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,15 +1,21 @@
-﻿//Задача 25: Напишите цикл, который принимает на вход два натуральных числа (A и B) и возводит число A в степень B.
-Console.Write("Введите число А: ");
-int a=Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите степень В для числа А: ");
-int degree=Convert.ToInt32(Console.ReadLine());
-int Exponentiation(int a, int degree)
+﻿internal class Program
 {
-  int result = 1;
-  for(int i=1; i <= degree; i++)
-  {
-    result = result * a;
-  }
-    return result;
+    private static void Main(string[] args)
+    {
+        //Задача 25: Напишите цикл, который принимает на вход два натуральных числа (A и B) и возводит число A в степень B.
+        Console.Write("Введите число А: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите степень В для числа А: ");
+        int degree = Convert.ToInt32(Console.ReadLine());
+        int Exponentiation(int a, int degree)
+        {
+            int result = 1;
+            for (int i = 1; i <= degree; i++)
+            {
+                result = result * a;
+            }
+            return result;
+        }
+        Console.Write($"{a} ^ {degree} = {Exponentiation(a, degree)}");
+    }
 }
-Console.Write($"{a} ^ {degree} = {Exponentiation(a, degree)}");
