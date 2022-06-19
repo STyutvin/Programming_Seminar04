837de55b8f85ff958ac1ee70bd2ac8b1d25815eb
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-19 12:00:16 +0300

    Задача №27 решена

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int SumNumber(int n)
+  {  
+    int counter = Convert.ToString(n).Length;
+    int reminder = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      reminder = n - n % 10;
+      result = result + (n - reminder);
+      n = n / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(n);
+Console.WriteLine("Сумма цифр в числе N: " + sumNumber);
