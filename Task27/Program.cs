1d4684396beea6d99356b1eecaee8037c1fe1eba
Sergey_Tyutvin <tyutvi@gmail.com>
2022-06-19 12:01:59 +0300

    Оформление кода в стиле С#

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,19 +1,26 @@
-﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-Console.Write("Введите число N: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int SumNumber(int n)
-  {  
-    int counter = Convert.ToString(n).Length;
-    int reminder = 0;
-    int result = 0;
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+        Console.Write("Введите число N: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int SumNumber(int n)
+        {
+            int counter = Convert.ToString(n).Length;
+            int reminder = 0;
+            int result = 0;
 
-    for (int i = 0; i < counter; i++){
-      reminder = n - n % 10;
-      result = result + (n - reminder);
-      n = n / 10;
+            for (int i = 0; i < counter; i++)
+            {
+                reminder = n - n % 10;
+                result = result + (n - reminder);
+                n = n / 10;
+            }
+            return result;
+        }
+
+        int sumNumber = SumNumber(n);
+        Console.WriteLine("Сумма цифр в числе N: " + sumNumber);
     }
-   return result;
-  }
-
-int sumNumber = SumNumber(n);
-Console.WriteLine("Сумма цифр в числе N: " + sumNumber);
+}
